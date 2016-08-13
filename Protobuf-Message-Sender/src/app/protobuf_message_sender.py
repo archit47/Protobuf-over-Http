b43a678b6faf501bec0580695998aa461db9b4ff
Archit Kapoor <archit@Archits-MacBook-Pro.local>
2016-08-14 01:57:08 +0530

    Adding a few methods available over a proto-message

--- Protobuf-Message-Sender/src/app/protobuf_message_sender.py
+++ Protobuf-Message-Sender/src/app/protobuf_message_sender.py
@@ -13,9 +13,18 @@ print employee_details
 #if employee_details.HasField('isBOEmployee'):
 #	print employee_details.isBOEmployee
 
-print employee_details.isBOEmployee
+print "Is the person a BO Employee? :", employee_details.isBOEmployee
 
 
+"""
+Check if all the 'required' fields are initialized or not.
+"""
+print "All required fields are initialized: ", employee_details.IsInitialized()
+
+
+#print "All fields in the proto-message: ", employee_details.ListFields()
+
+print "Byte Size of this proto-message:", employee_details.ByteSize()
 
 url = "http://localhost:8000"
 headers = {'Content-Type': 'application/x-protobuf', 'Accept': 'application/x-protobuf'}
@@ -26,7 +35,7 @@ try:
 	resp = ses.post(url, data=employee_details.SerializeToString())
 
 	print "Status Code of the response: " , resp.status_code
-	print "Response from Python-Service ", resp
+	print "Response from Python-Service ", resp.text
 
 except Exception as err:
 	print "Some error has occurred while sending the message"
