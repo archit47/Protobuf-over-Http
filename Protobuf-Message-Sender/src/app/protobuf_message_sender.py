dc02c36496cc98da420735ad10419deaf41633d2
Archit Kapoor <archit@Archits-MacBook-Pro.local>
2016-08-13 13:20:54 +0530

    Protobuf message sender over HTTP added

---
+++ Protobuf-Message-Sender/src/app/protobuf_message_sender.py
@@ -0,0 +1,32 @@
+from protobuf import Employee_pb2
+import requests
+
+employee_details = Employee_pb2.Employee()
+
+
+employee_details.name="Archit Kapoor"
+employee_details.email="xyz@gmail.com"
+employee_details.id=201
+
+print employee_details
+
+#if employee_details.HasField('isBOEmployee'):
+#	print employee_details.isBOEmployee
+
+print employee_details.isBOEmployee
+
+
+
+url = "http://localhost:8000"
+headers = {'Content-Type': 'application/x-protobuf', 'Accept': 'application/x-protobuf'}
+
+try:
+	ses = requests.Session()
+	ses.headers.update(headers)
+	r = ses.post(url, data=employee_details.SerializeToString())
+
+	print "Status Code received: " , r.status_code
+
+except Exception as err:
+	print "Some error has occurred while sending the message"
+	print err
